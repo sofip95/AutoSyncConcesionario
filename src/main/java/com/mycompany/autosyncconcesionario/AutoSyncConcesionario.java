e29b16f17736b2239c1ac2cdc15aea5ddaf2f9ed
Sofia <sofia.perez.vanegas@gmail.com>
2025-03-02 10:03:36 -0500

    Commit inicial

---
+++ src/main/java/com/mycompany/autosyncconcesionario/AutoSyncConcesionario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.autosyncconcesionario;
+
+/**
+ *
+ * @author sofia
+ */
+public class AutoSyncConcesionario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
