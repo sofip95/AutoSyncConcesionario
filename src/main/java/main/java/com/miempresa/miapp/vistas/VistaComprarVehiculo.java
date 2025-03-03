53558ef08957bf4d1709748ccbd807451ea39063
Juan Ceron <juanmiguelceronocampo@gmail.com>
2025-03-02 21:41:46 -0500

    REFACTOR nombre de la clase VistaCompraVehiculo to VistaComprarVehiculo

--- src/main/java/main/java/com/miempresa/miapp/vistas/VistaComprarVehiculo.java
+++ src/main/java/main/java/com/miempresa/miapp/vistas/VistaComprarVehiculo.java
@@ -8,12 +8,12 @@ package main.java.com.miempresa.miapp.vistas;
  *
  * @author JUAN
  */
-public class VistaCompraVehiculo extends javax.swing.JFrame {
+public class VistaComprarVehiculo extends javax.swing.JFrame {
 
     /**
      * Creates new form VistaCompraVehiculo
      */
-    public VistaCompraVehiculo() {
+    public VistaComprarVehiculo() {
         initComponents();
     }
 
@@ -140,20 +140,21 @@ public class VistaCompraVehiculo extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VistaCompraVehiculo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaComprarVehiculo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VistaCompraVehiculo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaComprarVehiculo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VistaCompraVehiculo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaComprarVehiculo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VistaCompraVehiculo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(VistaComprarVehiculo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new VistaCompraVehiculo().setVisible(true);
+                new VistaComprarVehiculo().setVisible(true);
             }
         });
     }
