65b200a61ac67768e4787c8e37f7602ef7a34201
Sofia <sofia.perez.vanegas@gmail.com>
2025-04-03 13:05:48 -0500

    ADDED changes to VehiculoService

---
+++ src/main/java/services/VehiculoService.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package services;
+
+import exceptions.InvalidVehiculoDataException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import DTO.Vehiculo;
+import repositories.VehiculoRepository;
+import validators.VehiculoValidator;
+
+/**
+ *
+ * @author sofia
+ */
+public class VehiculoService {
+
+    private VehiculoRepository vehiculoRepository = new VehiculoRepository();
+
+    public Vehiculo getVehiculoById(String placa) throws SQLException {
+        return vehiculoRepository.findById(placa);
+    }
+
+    public void createVehiculo(String placa, String marca, String modelo, String anio, String color, float precio_venta) throws
+            SQLException, InvalidVehiculoDataException {
+        if (!VehiculoValidator.validatePlaca(placa) || !VehiculoValidator.validateMarca(marca) || !VehiculoValidator.validateModelo(color) || !VehiculoValidator.validateAnio(anio) || !VehiculoValidator.validateColor(color) || !VehiculoValidator.validatePrecio(precio_venta)) {
+            throw new InvalidVehiculoDataException("Datos inválidos");
+        }
+
+        Vehiculo vehiculo = new Vehiculo(placa, marca, color, anio, color, precio_venta);
+
+        vehiculoRepository.save(vehiculo);
+    }
+    
+        public boolean updateVehiculo(String placa, String marca, String modelo, String anio, String color, float precio_venta) throws
+            SQLException, InvalidVehiculoDataException {
+        if (!VehiculoValidator.validatePlaca(placa) || !VehiculoValidator.validateMarca(marca) || !VehiculoValidator.validateModelo(color) || !VehiculoValidator.validateAnio(anio) || !VehiculoValidator.validateColor(color) || !VehiculoValidator.validatePrecio(precio_venta)) {
+           
+            throw new InvalidVehiculoDataException("Datos inválidos");
+        }
+
+        Vehiculo aux = vehiculoRepository.findById(placa);
+        if (aux == null) {
+            throw new InvalidVehiculoDataException("Vehiculo no encontrado");
+        }
+
+        Vehiculo vehiculoActualizado = new Vehiculo(placa, marca, modelo, anio, color, precio_venta);
+        vehiculoRepository.updateVehiculo(vehiculoActualizado);
+        return true;
+    }
+
+    public boolean deleteVehiculo(String placa) throws SQLException, InvalidVehiculoDataException {
+        Vehiculo vehiculoExistente = vehiculoRepository.findById(placa);
+        if (vehiculoExistente == null) {
+            throw new InvalidVehiculoDataException("Vehiculo no encontrado");
+        }
+
+        vehiculoRepository.deleteVehiculo(placa);
+        return true;
+    }
+    
+     public ArrayList<Vehiculo> listarVehiculos() throws SQLException{
+    return vehiculoRepository.listarVehiculos();
+    }
+}
