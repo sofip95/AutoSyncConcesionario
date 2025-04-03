dc452ec46f4206380d0ff39f8664182391c6399e
Juan Gallego <juanesteban.gallegol.02@gmail.com>
2025-04-03 18:03:23 -0500

    Add InvalidPruebaDataException

---
+++ src/main/java/exceptions/InvalidPruebaDataException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exceptions;
+
+/**
+ *
+ * @author Victus
+ */
+public class InvalidPruebaDataException extends Exception{
+    
+    public InvalidPruebaDataException(String message) {
+        super(message);
+    }
+}
